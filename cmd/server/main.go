1a3a099d50a503aaafa2e7425b653666d91c10d2
Connor Watson <connorwatson@Tammis-MacBook-Pro.local>
2026-04-02 15:12:57 -0500

    Add Dockerfile and environment variable configuration

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,50 +8,41 @@ import (
 	"github.com/MrBushido-002/chess-in-golang/internal/api"
 	"github.com/MrBushido-002/chess-in-golang/internal/db"
 	"github.com/joho/godotenv"
+	
 
 )
 
 func main() {
-	godotenv.Load()
-	
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "2209"
-	}
-	fmt.Println("Server starting on port", port)
-	log.Fatal(http.ListenAndServe(":"+port, mux))
+    godotenv.Load()
 
-	dbURL := os.Getenv("DB_URL")
+    dbURL := os.Getenv("DB_URL")
     conn, err := db.NewConnection(dbURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer conn.Close()
 
-	cfg := api.APIConfig{
-		DB: conn,
-		JWTSecret: os.Getenv("SECRET_KEY"),
-	}
+    cfg := api.APIConfig{
+        DB:        conn,
+        JWTSecret: os.Getenv("SECRET_KEY"),
+    }
 
-	fmt.Println("Database connected successfully!")
-	
-	mux := http.NewServeMux()
+    fmt.Println("Database connected successfully!")
 
-	mux.HandleFunc("POST /players/register", cfg.HandleRegisterPlayer)
-	mux.HandleFunc("POST /players/login", cfg.HandlePlayerLogin)
-	mux.HandleFunc("POST /games/", cfg.HandleCreateGame)
-	mux.HandleFunc("POST /games/{id}/join", cfg.HandleJoinGame)
-	mux.HandleFunc("POST /games/{id}/moves", cfg.HandelMakeMove)
-	mux.HandleFunc("GET /games/{id}", cfg.HandleGetGames)
-	mux.HandleFunc("GET /games/{id}/moves", cfg.HandelGetMoves)
-	mux.HandleFunc("GET /games/{id}/replay", cfg.HandleGetReplay)
+    mux := http.NewServeMux()
+    mux.HandleFunc("POST /players/register", cfg.HandleRegisterPlayer)
+    mux.HandleFunc("POST /players/login", cfg.HandlePlayerLogin)
+    mux.HandleFunc("POST /games", cfg.HandleCreateGame)
+    mux.HandleFunc("POST /games/{id}/join", cfg.HandleJoinGame)
+    mux.HandleFunc("POST /games/{id}/moves", cfg.HandelMakeMove)
+    mux.HandleFunc("GET /games/{id}", cfg.HandleGetGames)
+    mux.HandleFunc("GET /games/{id}/moves", cfg.HandelGetMoves)
+    mux.HandleFunc("GET /games/{id}/replay", cfg.HandleGetReplay)
 
-
-	server := &http.Server{
-		Addr: ":2209",
-		Handler: mux,
-	}
-
-	fmt.Println("Server starting on port 2209")
-	log.Fatal(server.ListenAndServe())
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "2209"
+    }
+    fmt.Println("Server starting on port", port)
+    log.Fatal(http.ListenAndServe(":"+port, mux))
 }
--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -7,7 +7,6 @@ import(
     "net/http"
     "errors"
     "strings"
-    "fmt"
 )
 
 
